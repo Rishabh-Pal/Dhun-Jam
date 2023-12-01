92e7c3b2717aad78f3a060728186d4743287c660
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-12-01 10:52:17 +0530

    styling of graph is done

--- vite-project/src/components/BarChart.jsx
+++ vite-project/src/components/BarChart.jsx
@@ -1,21 +1,39 @@
-import React from "react";
 
-const BarGraph = ({ regularAmounts }) => {
+import PropTypes from 'prop-types'
+// import "./BarChart.css"
+const BarGraph = ({ allAmounts }) => {
+  const maxAmount = Math.max(...allAmounts);
   return (
+    <>
     <div className="bar-graph">
-      {regularAmounts.map((value, index) => (
+    <div className='y-axis '>₹</div> 
+      {allAmounts.map((value, index) => (
         <div
           key={index}
-          className="bar"
+          className='bar'
           style={{
-            height: `${value * 3}px`,
+            height: `${(value/maxAmount) * 100}%`,
             border: "1px solid #333",
-            margin: "0 4px",
+            margin: "0 35px",
+            borderRadius: "5px"
           }}
         ></div>
       ))}
     </div>
+    <div className='x-axis'>
+      <span>category_6</span>
+      <span>category_7</span>
+      <span>category_8</span>
+      <span>category_9</span>
+      <span>category_10</span>
+
+    </div>
+    </>
   );
+};
+
+BarGraph.propTypes = {
+  allAmounts: PropTypes.arrayOf(PropTypes.number).isRequired,
 };
 
 export default BarGraph;
--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -1,19 +1,19 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import ChargeCustomers from "./ChargeCustomers";
-import CustomAmount from "./CustomAmount";
-import RegularAmounts from "./RegularAmounts";
 import BarGraph from "./BarChart";
-import { useEffect, useState } from "react";
 
-const Dashboard = () => { 
-    const [chargeCustomers, setChargeCustomers] = useState(false);
-    const [customAmount, setCustomAmount] = useState(0);
-    const [regularAmounts, setRegularAmounts] = useState([0, 0, 0, 0]);
-    const [isSaveEnabled, setIsSaveEnabled] = useState(false);
-    
-    const navigate = useNavigate();
-   
+const Dashboard = () => {
+  const [chargeCustomers, setChargeCustomers] = useState(false);
+  const [customAmount, setCustomAmount] = useState();
+  const [regularAmounts, setRegularAmounts] = useState([]);
+  const [allAmounts, setAllAmounts] = useState([]);
+  const [rerenderSignal, setRerenderSignal] = useState(false);
+  const [isSaveEnabled, setIsSaveEnabled] = useState(false);
+
+  const navigate = useNavigate();
+
   const areRegularAmountsValid = (amounts = regularAmounts) => {
     const minValues = [79, 59, 39, 19];
     return amounts.every((amount, index) => amount > minValues[index]);
@@ -23,7 +23,7 @@ const Dashboard = () => {
     setIsSaveEnabled(
       customAmount > 99 && areRegularAmountsValid(regularAmounts)
     );
-  }, [customAmount, regularAmounts]); 
+  }, [areRegularAmountsValid, customAmount, regularAmounts]);
 
   useEffect(() => {
     if (!localStorage.getItem("authToken")) {
@@ -31,12 +31,46 @@ const Dashboard = () => {
     }
   }, [navigate]);
 
+  const handleSave = async () => {
+    try {
+      const response = await fetch("https://stg.dhunjam.in/account/admin/4", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          amount: {
+            category_6: customAmount,
+            category_7: regularAmounts[0],
+            category_8: regularAmounts[1],
+            category_9: regularAmounts[2],
+            category_10: regularAmounts[3],
+          },
+        }),
+      });
+
+      if (response.ok) {
+        console.log("Settings saved:", {
+          chargeCustomers,
+          customAmount,
+          regularAmounts,
+        });
+        setRerenderSignal((prev) => !prev);
+      } else {
+        console.error("Failed to save settings:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error saving settings:", error);
+    }
+  };
+
   useEffect(() => {
     const fetchChargeCustomers = async () => {
       try {
         const response = await fetch("https://stg.dhunjam.in/account/admin/4");
         const data = await response.json();
-        console.log("this is the category data", data);
+        console.log("Category data:", data);
+
         setChargeCustomers(data.data.charge_customers);
         setCustomAmount(data.data.amount.category_6);
         setRegularAmounts([
@@ -45,7 +79,14 @@ const Dashboard = () => {
           data.data.amount.category_9,
           data.data.amount.category_10,
         ]);
-        // console.log(customAmount);
+        setAllAmounts([
+          data.data.amount.category_6,
+          data.data.amount.category_7,
+          data.data.amount.category_8,
+          data.data.amount.category_9,
+          data.data.amount.category_10,
+        ]);
+
         setIsSaveEnabled(
           data.data.amount.category_6 > 99 &&
             areRegularAmountsValid([
@@ -61,61 +102,63 @@ const Dashboard = () => {
     };
 
     fetchChargeCustomers();
-  }, []);
+  }, [rerenderSignal]);
 
-  const handleSave = async () => {
-    try {
-      const response = await fetch("https://stg.dhunjam.in/account/admin/4", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          // Add any other headers you need, including the authorization header with the token
-        },
-        body: JSON.stringify({
-          amount: {
-            category_6: customAmount,
-            category_7: regularAmounts[0],
-            category_8: regularAmounts[1],
-            category_9: regularAmounts[2],
-            category_10: regularAmounts[3],
-          },
-        }),
-      });
-
-      if (response.ok) {
-        console.log("Settings saved:", { chargeCustomers, customAmount, regularAmounts }); 
-      } else {
-        console.error("Failed to save settings:", response.statusText); 
-      }
-    } catch (error) {
-      console.error("Error saving settings:", error);
-    }
+  const handleRegularAmountChange = (index, value) => {
+    const newRegularAmounts = [...regularAmounts];
+    newRegularAmounts[index] = parseInt(value, 10) || 0;
+    setRegularAmounts(newRegularAmounts);
+    setIsSaveEnabled(
+      customAmount > 99 && areRegularAmountsValid(newRegularAmounts)
+    );
+  };
+  const handleCustomAmountChange = (value) => {
+    const amount = parseInt(value, 10) || 0;
+    setCustomAmount(amount);
+    setIsSaveEnabled(amount > 99 && areRegularAmountsValid());
   };
 
   return (
     <div>
       <h1 className="venue-name">Social, Hebbal on Dhun Jam</h1>
       <div className="settings-container">
-      <ChargeCustomers
+        <ChargeCustomers
           chargeCustomers={chargeCustomers}
           setChargeCustomers={setChargeCustomers}
         />
         {chargeCustomers && (
           <>
-            <CustomAmount
-              customAmount={customAmount}
-              setCustomAmount={setCustomAmount}
-              setIsSaveEnabled={setIsSaveEnabled}
-              areRegularAmountsValid={areRegularAmountsValid}
-            />
-           <RegularAmounts
-           customAmount={customAmount}
-              regularAmounts={regularAmounts}
-              setRegularAmounts={setRegularAmounts}
-              setIsSaveEnabled={setIsSaveEnabled}
-            />
-            <BarGraph regularAmounts={regularAmounts} />
-          </> 
+            <div className="setting-item">
+              <div className="text">Custom song request amount-</div>
+              <div>
+                <input
+                  type="number"
+                  className="custom-number-input"
+                  value={customAmount}
+                  onChange={(e) => handleCustomAmountChange(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="setting-item">
+              <div className="text">
+                Regular song request amounts from high to low-
+              </div>
+              <div className="regular-amounts">
+                {regularAmounts.map((value, index) => (
+                  <input
+                    key={index}
+                    type="number"
+                    className="custom-number-input"
+                    value={value}
+                    onChange={(e) =>
+                      handleRegularAmountChange(index, e.target.value)
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+            <BarGraph allAmounts={allAmounts} />
+          </>
         )}
       </div>
       <button onClick={handleSave} disabled={!isSaveEnabled}>
